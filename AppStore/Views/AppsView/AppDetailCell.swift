130f05fa3b5d27d38386456320e778bd1275bb3f
canerkarabulutt <ckarabulut55@gmail.com>
2024-03-06 14:12:28 +0300

    Detail process started

--- AppStore/Views/AppsView/AppDetailCell.swift
+++ AppStore/Views/AppsView/AppDetailCell.swift
@@ -5,4 +5,26 @@
 //  Created by Caner Karabulut on 6.03.2024.
 //
 
-import Foundation
+import UIKit
+
+class AppDetailCell: UICollectionViewCell {
+    //MARK: - Properties
+    //MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+//MARK: - Helpers
+extension AppDetailCell {
+    private func style() {
+        
+    }
+    private func layout() {
+        
+    }
+}
+
