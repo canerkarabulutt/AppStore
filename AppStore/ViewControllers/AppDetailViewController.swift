db512b6ebe9b01e418ff2b975c6e0db16ddc9a67
canerkarabulutt <ckarabulut55@gmail.com>
2024-03-06 14:16:56 +0300

    Details started

--- AppStore/ViewControllers/AppDetailViewController.swift
+++ AppStore/ViewControllers/AppDetailViewController.swift
@@ -5,4 +5,28 @@
 //  Created by Caner Karabulut on 6.03.2024.
 //
 
-import Foundation
+import UIKit
+
+class AppDetailViewController: UIViewController {
+    //MARK: - Properties
+    private let collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        return collectionView
+    }()
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+//MARK: - Helpers
+extension AppDetailViewController {
+    private func style() {
+        
+    }
+    private func layout() {
+        
+    }
+}
+
